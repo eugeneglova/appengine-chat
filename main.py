00af8bb6afe748e514ace9b514264519fe0a1c54
Eugene Glova <eugene.glova@gmail.com>
2014-06-12 17:36:30 +0400

    Create a simple channel

--- main.py
+++ main.py
@@ -14,13 +14,49 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+import jinja2
+import os
 import webapp2
+from google.appengine.api import channel
+from google.appengine.api import users
+
+class ChannelReceiveHandler(webapp2.RequestHandler):
+
+  def post(self):
+
+    msg = self.request.get('msg')
+
+    user = users.get_current_user()
+
+    channel.send_message("shared_channel", msg)
 
 class MainHandler(webapp2.RequestHandler):
-    def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Hello world!')
+  """This page is responsible for showing the game UI. It may also
+  create a new game or add the currently-logged in user to a game."""
+
+  def get(self):
+
+    user = users.get_current_user()
+
+    if not user:
+      self.redirect(users.create_login_url(self.request.uri))
+      return
+
+    token = channel.create_channel("shared_channel")
+
+    template_values = {
+        "token": token,
+        "nickname": user.nickname()
+    }
+
+    template = jinja_environment.get_template("index.html")
+    
+    self.response.out.write(template.render(template_values))
+
+jinja_environment = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ("/", MainHandler),
+    ("/_ah/channel/receive", ChannelReceiveHandler)
 ], debug=True)
