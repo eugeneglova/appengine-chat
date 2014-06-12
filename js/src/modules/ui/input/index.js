f8bd9223c44c9d601c8b09ed17c0c4b66e967425
Eugene Glova <eugene.glova@gmail.com>
2014-06-12 20:45:26 +0400

    Remove unneeded methods for input

--- js/src/modules/ui/input/index.js
+++ js/src/modules/ui/input/index.js
@@ -72,69 +72,6 @@ define(["backbone", "hbs!./templates/index"], function (Backbone, template) {
 
 
         /**
-         * onMessage
-         *
-         * When message is received
-         *  - transmit message via global bus
-         *
-         * @return {Boolean}
-         */
-        onMessage: function(msg) {
-            // Use Backbone as global bus to exchange messages with other modules
-            Backbone.trigger("service:message:received", msg.data);
-
-            console.info("Message Received: ", msg, ", Data:", msg.data);
-
-            return true;
-        },
-
-
-        /**
-         * onError
-         *
-         * When error is happened
-         *
-         * @return {Boolean}
-         */
-        onError: function(e) {
-            console.info("CHANNEL Error. Code:", e.code, ", Description: ", e.description);
-
-            return true;
-        },
-
-
-        /**
-         * onClose
-         *
-         * When socket is closed
-         *
-         * @return {Boolean}
-         */
-        onClose: function() {
-            console.info("Close Channel");
-
-            return true;
-        },
-
-
-        /**
-         * onSend
-         *
-         * When other module sends us a message
-         *
-         * @return {Boolean}
-         */
-        onSend: function(msg) {
-            // Send a message
-            this.socket.send(msg);
-
-            console.info("Message Sent:", msg);
-
-            return true;
-        },
-
-
-        /**
          * _sendMessage
          *
          * Sends a message
